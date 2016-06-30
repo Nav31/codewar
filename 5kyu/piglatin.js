a535f132cf5cabadfb31ace7cb780d82dd1a76aa
Nav31 <nsingh3104@gmail.com>
2016-06-30 12:23:08 -0400

    completed pigLatin

---
+++ 5kyu/piglatin.js
@@ -0,0 +1,12 @@
+var pigIt = (str) => {
+	let holder = ''
+	const ay = 'ay';
+	let arr = str.split(" ")
+	for(var i = 0; i < arr.length; i++) {
+		let firstLetter = arr[i].slice(0,1)
+		arr[i] += firstLetter + ay
+		holder += (arr[i].slice(1,arr[i].length))
+		holder += " "
+	}
+	return holder.trim()
+}
---
+++ 6kyu/deleteNth.js
@@ -0,0 +1,13 @@
+
+
+var deleteNth = (arr, n) => {
+	let holder = [];
+	for(var i = 0; i < arr.length; i++) {
+		let counter = 0;
+		for(var j = 1; j < arr.length; j++) {
+			if(arr[i] === arr[j]) {
+				counter++
+			}
+		}
+	}	
+}
