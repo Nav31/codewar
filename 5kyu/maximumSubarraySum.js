fe62c1e5abd2b6f1664347c05e1476091ed608a8
Nav31 <nsingh3104@gmail.com>
2016-07-03 11:53:42 -0400

    problem completed

--- 5kyu/maximumSubarraySum.js
+++ 5kyu/maximumSubarraySum.js
@@ -1,21 +1,10 @@
-// https://www.codewars.com/kata/maximum-subarray-sum/train/javascript
-
 var maxSequence = arr => {
-	let holder = [];
-	let runningSum = Number.NEGATIVE_INFINITY;
-	for (var i = 0; i < arr.length; i++) {
-		let n = 1;
-		for (var j = arr.length; j > 0; j--) {
-			var subArray = arr.slice(i, j+n)
-			if(subArray.length === 0) break 
-			else {
-				var subArraySum = subArray.reduce((prev, curr) => prev + curr);
-				if(subArraySum > runningSum) {
-					runningSum = subArraySum;
-					n++;
-				}	
-			}
+	let max = 0;
+	for(var i = 0; i < arr.length; i++) {
+		for(var j = arr.length; j > i; j--) {
+			let total = arr.slice(i, j).reduce((prev, curr) => prev + curr);
+			if(total > max) max = total;
 		}
 	}
-	return runningSum;
+	return max;
 }
