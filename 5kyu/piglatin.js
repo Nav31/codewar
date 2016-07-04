27ea1d177c4ea3773f8ec0b2ed8e3b5b19ed9e64
Nav31 <nsingh3104@gmail.com>
2016-07-04 14:15:50 -0400

    reverted file

--- 5kyu/piglatin.js
+++ 5kyu/piglatin.js
@@ -10,15 +10,3 @@ var pigIt = (str) => {
 	}
 	return holder.trim()
 }
-
-
-var newPig = str => {
-    let newHolder = "";
-    const ay = 'ay';
-    let arr = str.split(" ");
-    for(var i = 0; i < arr.length; i++) {
-        let firstLetter = arr[i].spice(0,1);
-        arr[i] += firstLetter + ay;
-        newHolder
-    }
-}
