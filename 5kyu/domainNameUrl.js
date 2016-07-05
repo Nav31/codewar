44c90f5b14e658365173918c1651d5f0c71b25b4
Nav31 <nsingh3104@gmail.com>
2016-07-04 20:16:48 -0400

    updated solution

--- 5kyu/domainNameUrl.js
+++ 5kyu/domainNameUrl.js
@@ -1,15 +1,17 @@
 //https://www.codewars.com/kata/extract-the-domain-name-from-a-url-1/train/javascript
 
+var domainName = url => url.match(/(?:http(?:s)?:\/\/)?(?:w{3}\.)?([^\.]+)/i)[1];
+
+// or this...
+
 var domainName = url => {
 	let re = /[a-zA-Z0-9]|\W+/g;
 	let matches = url.match(re);
 	let holder = "";
 	for(var i = 0; i < matches.length; i++) {
 		if(matches[i] === '://' ) {
-			// check if the next things are 'www.' if not do what is below 
-			// else do what is below start after the www.
-			if(matches[i+4] === '.') {	// to see if after the '://' is 'www.'
-				for(var k = i + 5; k < matches.length; k++)
+			if(matches[i+3] === '.') {	// to see if after the '://' is 'www.'
+				for(var k = i + 4; k < matches.length; k++)
 					if(matches[j] !== '.') holder += matches[j]
 					else break;
 			} else {
@@ -19,6 +21,18 @@ var domainName = url => {
 				} 
 			}
 		} 
+		if(matches[i] === 'w' && matches[i+1] === 'w' && matches[i+3] === '.') {
+			for(var l = i+4; l < matches.length; l++) {
+				if(matches[l] !== '.') holder += matches[l]
+				else break;
+			}
+		} 
+		else {
+      		for(var m = 0; m < matches.length; m++) {
+        		if(matches[m] !== '.') holder += matches[m]
+        		else break
+      		}
+    	}
 	}
 	return holder;
 }
