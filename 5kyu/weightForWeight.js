3c27624dac4d7c37b59f02f826bf356d8fa28161
Nav31 <nsingh3104@gmail.com>
2016-07-08 17:28:32 -0400

    completed

--- 5kyu/weightForWeight.js
+++ 5kyu/weightForWeight.js
@@ -1,13 +1,11 @@
-// https://www.codewars.com/kata/weight-for-weight/train/javascript
 var orderWeight = string => {
 	if(string.length === 0) return "";
 	let weightArr = string.split(" ").sort();
-	weightArr.sort()
 	let holder = {}, anwsArr = [], str = "", multiArr = [], another = [], finalStr = "";
 	for(let i = 0; i < weightArr.length; i++) {
 		holder[weightArr[i]] = (weightArr[i].split("").reduce((prev, curr) => parseInt(prev) + parseInt(curr)));
 	}
-	for(let weight in holder) {anwsArr.push([weight, holder[weight]])}
+	for(let weight in holder) { anwsArr.push([weight, holder[weight]]) }
 	anwsArr = anwsArr.sort((a,b) => a[1] - b[1]);
 	for(let i = 0; i < anwsArr.length - 1; i++) {
 		if(parseInt(anwsArr[i][1]) === parseInt(anwsArr[i+1][1])) { 
@@ -25,16 +23,16 @@ var orderWeight = string => {
 		}
 	}
 	anwsArr.forEach(ele => str += ele[0] + " ");
-	for(var j = 0; j < weightArr.length; j++) {
+	for(let j = 0; j < weightArr.length; j++) {
 		let count = 0;		// getting duplicate values
-		for(var k = 0; k < weightArr.length; k++) { if(weightArr[j] === weightArr[k]) count++ }
+		for(let k = 0; k < weightArr.length; k++) { if(weightArr[j] === weightArr[k]) count++ }
 		if (count >= 2) multiArr.push(weightArr[j])
 	}
 	multiArr = multiArr.sort((a,b) => a-b);
 	multiArr = multiArr.filter((item, pos) => multiArr.indexOf(item) == pos)
 	str = str.trim();
 	let finalArr = str.split(" ");
-	for(var i = 0; i < multiArr.length; i++) {
+	for(let i = 0; i < multiArr.length; i++) {
 		let idx = finalArr.indexOf(multiArr[i]);
 		if(idx > -1) finalArr.splice(idx, 0, multiArr[i])
 	}
