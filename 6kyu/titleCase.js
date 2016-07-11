f238a3c6a3693585fe5ab2468ddd33a345e69574
Nav31 <nsingh3104@gmail.com>
2016-07-11 16:16:22 -0400

    complete

--- 6kyu/titleCase.js
+++ 6kyu/titleCase.js
@@ -1,24 +1,17 @@
 // https://www.codewars.com/kata/title-case/train/javascript
 
-
-
-
 var titleCase = (title, minorWords) => {
-	let firstUpper = arr => arr.map(ele => ele.charAt(0).toUpperCase() + ele.slice(1))
-	if(title.length === 0) return '';
-	let splitTitle = title.split(" ");
-	splitTitle = splitTitle.map(ele => ele.toLowerCase());
-	if(minorWords === undefined) return firstUpper(splitTitle)
-	else var minorWs = minorWords.split(" ");
-	splitTitle = firstUpper(splitTitle);
-	minorWs = firstUpper(minorWs)
-	let holder = [];
-	minorWs.forEach(ele => {
-		splitTitle.forEach(word => {
-			if (ele === word) {
-				holder.push(word.charAt(0).toLowerCase() + word.slice(1))
-			} else holder.push(word)
-		})
-	})
-	return holder.slice().join(" ")
+  	if(title === "") return "";
+	title = title.toLowerCase().split(" ");
+	let firstUpper = arr => arr.map(ele => ele.charAt(0).toUpperCase() + ele.slice(1));
+	title = firstUpper(title);
+  	if(minorWords === undefined) return title.join(" ");
+  	minorWords = minorWords.toLowerCase().split(" ");
+	minorWords = firstUpper(minorWords);
+	for(let i = 1; i < title.length; i++) {
+		if(minorWords.indexOf(title[i]) > -1) {
+			title[i] = title[i].charAt(0).toLowerCase() + title[i].slice(1);
+		}
+	}
+	return title.join(" ");
 }
