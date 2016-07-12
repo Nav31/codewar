e34ffd4956ee75f526829c0b3a102e7c939c8a88
Nav31 <nsingh3104@gmail.com>
2016-07-12 12:07:10 -0400

    complete

--- 6kyu/findTheOddInt.js
+++ 6kyu/findTheOddInt.js
@@ -1,11 +1,11 @@
-
-
 var findOdd = arr => {
-	let evenOrOdd = num => n % 2 === 0 ? true : false;
+	let evenOrOdd = num => num % 2 === 0 ? true : false;
 	let count = 0, holder = {};
 	for(let i = 0; i < arr.length; i++) {
 		if(holder[arr[i]]) holder[arr[i]] ++
 		else holder[arr[i]] = 1;
 	}
-	console.log(holder)
+	for(let key in holder) {
+		if(!evenOrOdd(holder[key])) return parseInt(key)
+	}
 }
