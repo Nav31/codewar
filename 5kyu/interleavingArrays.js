cb11201009b5c344977ccabb24ca400088406958
Nav31 <nsingh3104@gmail.com>
2016-07-18 20:45:37 -0400

    complete

--- 5kyu/interleavingArrays.js
+++ 5kyu/interleavingArrays.js
@@ -3,14 +3,9 @@ function interleave () {
 	let args = Array.prototype.slice.call(arguments);
 	if(args.length === 0 || args[0] === null) return [];
 	let doSomething = arr => {
-		for(let i = 0; i < arr.length; i++) {
-			holder.push(arr[i][count]);
-		}
+		arr.forEach(ele => holder.push(ele[count]));
 		count++;
-		return args;
 	}
-	while(count < args[0].length) {
-		doSomething(args);
-	}
+	while(count < args[0].length) doSomething(args);
 	return holder;
 }
