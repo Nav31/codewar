f28dac33df4b6b3533e2867de764123dd3294ca3
Nav31 <nsingh3104@gmail.com>
2016-07-20 18:02:39 -0400

    complete

---
+++ 4kyu/removingZeros.js
@@ -0,0 +1,18 @@
+// can't use array or obj methods -__- / hence all the code;
+var removeZeros = arr => {
+    let holder = 0, last = arr[arr.length - 1], numbersOfZeros = 0;
+    for(let i = 0; i< arr.length; i++) {
+        if(arr[i] === 0 || arr[i] === '0') numbersOfZeros++;
+    }
+    for(let i = 0; i < arr.length; i++) {
+    if(!numbersOfZeros) break;
+		if(arr[i] === 0 || arr[i] === '0') {
+			holder = arr[i];
+			for(let j = i; j < arr.length; j++) arr[j] = arr[j+1];
+			arr[arr.length - 1] = holder
+			i--;
+      		numbersOfZeros--;
+		}
+	}
+  	return arr
+}
