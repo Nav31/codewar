0b8243474dac45a743735f050a93aa6fdbea25f0
Nav31 <nsingh3104@gmail.com>
2016-07-20 12:00:30 -0400

    complete

---
+++ 4kyu/stripComments.js
@@ -0,0 +1,19 @@
+var solution = (input, markers) => {
+	let arr = input.split("\n");
+	let splitHolder = [], str = "";
+	arr.forEach(ele => splitHolder.push(ele.split("")));
+	for(let i = 0; i < splitHolder.length; i++) {
+		for(let j = 0; j < markers.length; j++) {
+			let idx = splitHolder[i].indexOf(markers[j]);
+			if(idx > -1) splitHolder[i].splice(idx-1, splitHolder[i].length);
+		}
+	}
+	for(let i = 0; i < splitHolder.length; i++) {
+		let joinedStr = splitHolder[i].join("");
+		str += joinedStr + '\n';
+	}
+	str = str.split("").slice(0, str.length-1);
+	return str.join("");
+}
+
+solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]);
