7b9cb80bead6f3f780b21aab6259a5832e748d16
Nav31 <nsingh3104@gmail.com>
2016-07-30 21:45:49 -0400

    making progress

--- 5kyu/ticTacToeChecker.js
+++ 5kyu/ticTacToeChecker.js
@@ -1,12 +1,25 @@
-
-
 var isSolved = (arr) => {
-	// This checks in any horizonatal winners
+	// checks in board is not yet solved / this could go last since 0's are empty space
 	for(let i = 0; i < arr.length; i++) {
-		if(arr[i][0] >= 1 && arr[i][0] === arr[i][1] && arr[i][1] === arr[i][2]) return arr[i][0];
+		for(let j = 0; j < arr.length; j++) {
+			if(arr[i][j] === 0) return -1;
+		}
 	}
-	// This is going to check vertical;
+	// make a function that takes in an arr and checks in all nums are the same
+	let allSame = someArr => {
+		for(let i = 0, j = i+1; i < someArr.length - 1; i++) {
+			if(someArr[i] !== someArr[j]) return false;
+		}
+		return true;
+	}
+	// now go in and check if all horizontals are the same in the grid
 	for(let i = 0; i < arr.length; i++) {
-		let temp = []
+		if(allSame(arr[i])) return arr[i][0];
+	}
+	// now check all verticals are the same in the grid
+	for(let i = 0; i < arr[0].length; i++) {
+		let holder = [];
+		holder.push(arr[0][i], arr[1][i], arr[2][i]);
+		if(allSame(holder)) return arr[0][i];
 	}
 }
