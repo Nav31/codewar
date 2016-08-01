a3cf122709074176b18b1d54bf08c628dc3f902c
Nav31 <nsingh3104@gmail.com>
2016-07-31 21:01:47 -0400

    almost done

--- 6kyu/sumConsecutives.js
+++ 6kyu/sumConsecutives.js
@@ -1,11 +1,13 @@
 
 
-var sumConsecutives = arr => {
-	let count = 0, holder = [];
-	for(let i = 0 j = 1; i < arr.length - 1; i++) {
-		while(arr[i] === arr[count]) {
-			
+const sumConsecutives = arr => {
+	let holder = [], count = 0;
+	for(let i = 0, j=i+1; i < arr.length; i++) {
+		if(arr[i] === arr[j]) count += arr[i]
+		else {
+			holder.push(arr[i])
+			if(count > 0) holder.push(count)
 		}
 	}
-	return holder;
+	return count;
 }
