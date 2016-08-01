9042aaa24d14350932b258a97abbb9c3fa519421
Nav31 <nsingh3104@gmail.com>
2016-08-01 19:34:03 -0400

    complete

--- 6kyu/theRangeFunction.js
+++ 6kyu/theRangeFunction.js
@@ -1,7 +1,7 @@
 function range (start, stop, step) {
 	let holder = [], args = Array.prototype.slice.call(arguments);
 	if(args[2] === 0) for(let i = start; i < stop; i++) holder.push(1);
-	else if(args.length === 1) for(let i = 0; i < args[0]; i++) holder.push(i)
+	else if(args.length === 1) for(let i = 0; i < args[0]; i++) holder.push(i);
 	else if(args.length === 3) for(let i = start; i < stop; i += step) holder.push(i);
 	else for(let i = start; i < stop; i++) holder.push(i);
 	return holder;
