06a64fe6bf081d356382730da59866970f7dd980
Nav31 <nsingh3104@gmail.com>
2016-08-02 14:13:53 -0400

    complete

--- 6kyu/sumConsecutives.js
+++ 6kyu/sumConsecutives.js
@@ -1,17 +1,15 @@
-// https://www.codewars.com/kata/sum-consecutives/train/javascript
-
 const sumConsecutives = arr => {
 	let holder = [], count = 0;
 	for(let i = 0; i < arr.length; i++) {
-		if(arr[i] === arr[i+1]) count += arr[i];
+		if(arr[i] === arr[i+1]) count += arr[i]
 		else {
-			if(count > 0) {
-				holder.push(count)
+			if(count > -10000) {
+				count += arr[i];
+				holder.push(count);
 				count = 0;
-			}
-			holder.push(arr[i]);
+			} else holder.push(arr[i]);
 		}
 	}
 	return holder;
 }
-sumConsecutives([1,4,4,4,0,4,3,3,1])
+sumConsecutives([1,4,4,4,0,4,3,3,1]);
