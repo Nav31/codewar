13c1c9d678cdc29e018125a050ed85a7ba243624
Nav31 <nsingh3104@gmail.com>
2016-08-07 11:21:41 -0400

    complete

--- 6kyu/characterFrequency.js
+++ 6kyu/characterFrequency.js
@@ -1,16 +1,6 @@
-// http://www.codewars.com/kata/character-frequency-1/train/javascript
-// const letterFrequency = str => {
-// 	let holder = {}, anws = [];
-// 	let arr = str.toLowerCase().replace(/\W/g,"").split(" ").join("").split("").sort();
-// 	arr.forEach(elem => elem in holder ? holder[elem]++ : holder[elem] = 1);
-// 	let temp = Object.keys(holder).sort((a,b) => holder[b] - holder[a]);
-// 	temp.forEach(elem => elem in holder ? anws.push([elem, holder[elem]]) : null);
-// 	return anws;
-// }
-
 const letterFrequency = str => {
 	let holder = {}, anws = [], something = {};
-	let arr = str.toLowerCase().replace(/\W/g,"").split(" ").join("").split("").sort();
+	let arr = str.toLowerCase().replace(/[^a-zA-Z]/g,"").split("").sort();
 	arr.forEach(elem => elem in holder ? holder[elem]++ : holder[elem] = 1);
 	let temp = Object.keys(holder).sort((a,b) => holder[b] - holder[a]);
 	for(let key in holder) {
@@ -20,7 +10,7 @@ const letterFrequency = str => {
 	for(let key in something) something[key].sort();
 	for(let key in something) {
 		while(something[key].length !== 0) {
-			anws.push([something[key].shift(), parseInt(key)]);
+			anws.push([something[key].pop(), parseInt(key)]);
 		}
 	}
 	return anws.reverse()
