318073ebebfec8acd759e3dc92caa20823b0b64b
Nav31 <nav31@users.noreply.github.com>
2016-08-25 11:48:55 -0400

    complete

--- 5kyu/JosephusPermutation.js
+++ 5kyu/JosephusPermutation.js
@@ -1,14 +1,11 @@
-// http://www.codewars.com/kata/josephus-permutation/train/javascript
-
 const josephus = (arr, k) => {
+	if(arr.length <= 1) return arr;
 	let answ = [], idx = k-1;
 	while(arr.length !== 0) {
+		if(idx > arr.length-1) idx = (idx % arr.length);
 		answ.push(arr[idx]);
 		arr.splice(idx, 1);
 		idx = idx + k-1;
-		if(idx > arr.length) idx = idx % arr.length;
 	}
 	return answ;	
 }
-
-josephus([1,2,3,4,5,6,7],3)
