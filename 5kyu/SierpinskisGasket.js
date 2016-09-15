0fb690302998d90645278f080bc0ec6e99c726ce
Nav31 <nav31@users.noreply.github.com>
2016-09-14 21:09:08 -0400

    working on it

---
+++ 5kyu/SierpinskisGasket.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/sierpinskis-gasket/train/javascript
+
+const sierpinski = n => {
+    const base = 'L'
+    let iter = 0;
+    while(iter < n) {
+
+    }
+    
+}
---
+++ 5kyu/vectorClass.js
@@ -0,0 +1,41 @@
+var Vector = function (components) {
+  // TODO: Finish the Vector class.
+  this.nums = components;
+  this.answ = [];
+  this.sum = 0;
+  this.holder = ['(']
+};
+Vector.prototype.add = function (arr) {
+    if(this.nums.length !== arr.nums.length) throw Error;
+    for(let i = 0; i < arr.nums.length; i++) {
+        this.answ.push(this.nums[i] + arr.nums[i])
+    }
+    return this.answ;
+} 
+Vector.prototype.subtract = function (arr) {
+    if(this.nums.length !== arr.nums.length) throw Error;
+    for(let i = 0; i < arr.nums.length; i++) {
+        this.answ.push(this.nums[i] - arr.nums[i])
+    }
+    return this.answ;
+}
+Vector.prototype.dot = function (arr) {
+    if(this.nums.length !== arr.nums.length) throw Error;
+    for(let i = 0; i < arr.nums.length; i++) {
+        this.sum += this.nums[i] * arr.nums[i];
+    }
+    return this.sum;
+}
+Vector.prototype.norm = function () {
+    this.nums.forEach(elem => this.sum += Math.pow(elem, 2));
+    return Math.sqrt(this.sum);
+}
+Vector.prototype.toString = function () {
+    this.nums.forEach(elem => this.holder.push(elem, ","));
+    this.holder.pop();
+    this.holder.push(')');
+    return this.holder.join("");
+}
+Vector.prototype.equals = function (obj) {
+    return this.nums === obj.nums;
+}
