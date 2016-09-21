a5649c58bd3a7bda235ab7ae61e4ce4797621f9f
Nav31 <nav31@users.noreply.github.com>
2016-09-21 16:43:14 -0400

    complete

--- 4kyu/sumIntervals.js
+++ 4kyu/sumIntervals.js
@@ -1,9 +1,13 @@
-// https://www.codewars.com/kata/sum-of-intervals/train/javascript
-// Write a function called sumIntervals that accepts an array of intervals, 
-// and returns the sum of all the interval lengths. 
-// Overlapping intervals should only be counted once.
-
-
+const sumIntervals = intervals => {
+	let holder = [], obj = {};
+	for(let i = 0; i < intervals.length; i++) {
+		for(let j = intervals[i][0]; j < intervals[i][1]; j++) {
+			holder.push(j)
+		}
+	}
+	holder.forEach(elem => elem in obj ? obj[elem]++ : obj[elem] = []);
+	return Object.keys(obj).length
+}
 function sumIntervals(intervals){
   	var holder = [];
   	intervals.forEach(ele => {
