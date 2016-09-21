3d5add8b868b14d4c3b08e4ddeefff4edd0627c9
Nav31 <nav31@users.noreply.github.com>
2016-09-21 18:15:13 -0400

    complete

--- 4kyu/pickPeaks.js
+++ 4kyu/pickPeaks.js
@@ -1,14 +1,20 @@
 const allSame = arr => arr.filter((elem, pos) => arr.indexOf(elem) === pos).length === 1;
 const isPlateu = arr => {
 	let first = arr.shift(), last = arr.pop();
-	return (allSame(arr) && first < arr[0] && arr[0] > last) ? true : false;
+	if(allSame(arr)) {
+		if(first < arr[0] && arr[0] > last) return true
+	}
+	return false;
 }
 const pickPeaks = arr => {
 	let obj = {"pos": [], "peaks": []};
-	for(let i = 1; i < arr.length; i++) {
+	for(let i = 1; i < arr.length - 1; i++) {
 		if(arr[i-1] < arr[i] && arr[i] >= arr[i+1]) {
-			obj["pos"].push(i);
-			obj["peaks"].push(arr[i]);
+			if(arr[i] > arr[i+2]) {
+				obj["pos"].push(i);
+				obj["peaks"].push(arr[i]);
+			}
+
 		}
 	}
 	for(let i = 0; i < arr.length - 1; i++) {
