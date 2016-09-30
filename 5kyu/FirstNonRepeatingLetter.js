6150eab899392cb9de05358e873ceeeb30ca4e40
Nav31 <nav31@users.noreply.github.com>
2016-09-29 21:07:49 -0400

    complete

--- 5kyu/FirstNonRepeatingLetter.js
+++ 5kyu/FirstNonRepeatingLetter.js
@@ -1,7 +1,6 @@
 function firstNonRepeatingLetter(s) {
 	if(s.length === 1) return s;
-    let sCopy = s;
-    let wordArr = sCopy.toLowerCase().split(""), letterObj = {}, getIdx;
+    let wordArr = s.toLowerCase().split(""), letterObj = {}, getIdx;
     wordArr.forEach((elem, idx) => elem in letterObj ? letterObj[elem][0]++ : letterObj[elem] = [1, idx]);
     for(let key in letterObj) {
         if(letterObj[key][0] === 1) {
@@ -9,6 +8,6 @@ function firstNonRepeatingLetter(s) {
         	break;
         }
     }
-    if(getIdx) return sCopy.split("")[getIdx];
+    if(getIdx) return s.split("")[getIdx];
     else return "";
 }
