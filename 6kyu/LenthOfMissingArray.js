85aa6bf934baca26226470108a268c6caaf2c294
Nav31 <nav31@users.noreply.github.com>
2016-10-04 11:06:13 -0400

    complete

---
+++ 6kyu/LenthOfMissingArray.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/length-of-missing-array/train/javascript
+function getLengthOfMissingArray(arrayOfArrays) {
+    let obj = {}, ans = 0,returnFalseRN = false;
+    if(!Array.isArray(arrayOfArrays)) return 0;
+    arrayOfArrays.forEach(elem => !Array.isArray(elem) || elem.length === 0 ? returnFalseRN = true : null);
+    if(returnFalseRN) return 0;
+    arrayOfArrays.forEach(elem => elem.length in obj ? null : obj[elem.length] = 1);
+    let keyArr = Object.keys(obj).map(elem => parseInt(elem)).sort((a,b) => a-b);
+    for(let i = 1; i < keyArr[keyArr.length - 1]; i++) {
+      if(keyArr.indexOf(i) === -1) ans = i;
+    }
+    return ans;
+}
