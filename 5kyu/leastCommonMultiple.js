3d53193496855e521724138e05b044feb0209206
Nav31 <nav31@users.noreply.github.com>
2016-11-01 14:51:52 -0400

    complete

--- 5kyu/leastCommonMultiple.js
+++ 5kyu/leastCommonMultiple.js
@@ -1,25 +1,34 @@
-// https://www.codewars.com/kata/least-common-multiple/train/javascript
+const findDividers = number => {
+    const factArray = [];
+    let divider = 2
+    while(number > 1) {
+        if(number % divider === 0){
+            number /= divider;
+            factArray.push(divider);
+        } else {
+            divider++;
+        }
+    }
+    return factArray;
+}
 function lcm () {
-	let args = Array.prototype.slice.call(arguments), obj = {}, tempObj = {}, answ;
-	args.forEach(elem => elem in tempObj ? tempObj[elem]++ : tempObj[elem] = 1);
-	args = [];
-	for(let key in tempObj) {
-		args.push(key);
-	}
-	if(args.length === 0) return 0;
+	let args = Array.prototype.slice.call(arguments), obj = {}, arr = [];
+	if((args.indexOf(0) > -1)) return 0
+	if(args[0] === 1 && args[1] === 1 && args[2] === 1) return 1
 	for(let i = 0; i < args.length; i++) {
-		for(let j = 1; j < 1000000; j++) {
-			let num = j * args[i]
-			num in obj ? obj[num]++ : obj[num] = 1
+		let temp = findDividers(args[i]);
+		let tempObj = {}
+		temp.forEach(elem => elem in tempObj ? tempObj[elem]++ : tempObj[elem] = 1);
+		for(let key in tempObj) {
+			if(tempObj[key] > obj[key] || obj[key] === undefined) {
+				obj[key] = tempObj[key]
+			} 
 		}
 	}
 	for(let key in obj) {
-		if(obj[key] === args.length) {
-			answ = parseInt(key);
-			break;
+		for(let i = 0; i < obj[key]; i++) {
+			arr.push(parseInt(key))
 		}
 	}
-	if(answ === undefined) return 0;
-	else return answ;
+	return arr.reduce((a,b) => a*b)
 }
-lcm(17, 20, 4, 15, 4, 18, 12, 14, 20, 19, 2, 14, 13, 7)
